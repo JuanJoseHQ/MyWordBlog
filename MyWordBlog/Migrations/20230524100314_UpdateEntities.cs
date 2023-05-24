6d62d61e6e1eb77fbe22bb8e7da6b882f74cd142
Juan José <juanjose2815@gmail.com>
2023-05-24 05:03:56 -0500

    Migration Entities.

---
+++ MyWordBlog/Migrations/20230524100314_UpdateEntities.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWordBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Comentaries",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Comentaries");
+        }
+    }
+}
