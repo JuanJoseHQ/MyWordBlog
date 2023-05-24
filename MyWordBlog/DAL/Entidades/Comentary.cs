ffb7766d6cc30024b846d10b9aee605cbfd57675
Juan José <juanjose2815@gmail.com>
2023-05-24 05:00:19 -0500

    Update Entitites

--- MyWordBlog/DAL/Entidades/Comentary.cs
+++ MyWordBlog/DAL/Entidades/Comentary.cs
@@ -5,11 +5,12 @@ namespace MyWordBlog.DAL.Entidades
 {
     public class Comentary : Entity
     {
-        [Display(Name = "Comentario")]
         [MaxLength(250, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime? DatePublication { get; set; }
+
+        public string? Author { get; set; }
 
         public Post Post { get; set; }
 
---
+++ MyWordBlog/Migrations/20230524095259_CreateEntities.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWordBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Desctipton = table.Column<string>(type: "nvarchar(3000)", maxLength: 3000, nullable: false),
+                    CantLikes = table.Column<int>(type: "int", nullable: true),
+                    CantDisLikes = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comentaries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    DatePublication = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    PostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comentaries_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentaries_PostId",
+                table: "Comentaries",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Title",
+                table: "Posts",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentaries");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
