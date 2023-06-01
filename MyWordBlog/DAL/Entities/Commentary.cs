66485ef6794ca0fd59326da1442256d7bba46250
Juan José <juanjose2815@gmail.com>
2023-05-31 20:32:15 -0500

    New Migration And Fixind Errors

--- MyWordBlog/DAL/Entities/Commentary.cs
+++ MyWordBlog/DAL/Entities/Commentary.cs
@@ -11,9 +11,9 @@ namespace MyWordBlog.DAL.Entidades
       
         public string? Author { get; set; }
 
-        public Post Post { get; set; }
+        public Post? Post { get; set; }
 
-        public Guid PostId { get; set; }
+        public Guid? PostId { get; set; }
 
     }
 }
---
+++ MyWordBlog/Migrations/20230601012953_NewChangesEntities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWordBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewChangesEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commentaries_Posts_PostId",
+                table: "Commentaries");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Commentaries",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commentaries_Posts_PostId",
+                table: "Commentaries",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commentaries_Posts_PostId",
+                table: "Commentaries");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Commentaries",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commentaries_Posts_PostId",
+                table: "Commentaries",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
